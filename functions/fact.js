67e48b33840392b5c60a5cd5c192b981d3f8bfb7
Dan Fone <dan@hammerheadrabbits.com>
2024-01-25 10:05:10 +0000

    async misstructured doh

--- functions/fact.js
+++ functions/fact.js
@@ -1,8 +1,6 @@
-export function onRequestGet() {
-  const returnedFact = async ()  => {
-    const fact = await fetch('https://catfact.ninja/fact');
-    const myJson = await fact.json();
-    console.log('myJson is ', myJson);
-    return myJson;
-  }
+export async function onRequestGet() {
+  const fact = await fetch('https://catfact.ninja/fact');
+  const myJson = await fact.json();
+  console.log('myJson is ', myJson);
+  return myJson;
 }
