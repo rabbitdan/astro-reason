4dbefe022d6d394028629b62ab8f2fbfc6674e29
Dan Fone <dan@hammerheadrabbits.com>
2024-01-25 15:51:38 +0000

    broken down vanilla version

--- functions/index.js
+++ functions/index.js
@@ -19,12 +19,14 @@ addEventListener('fetch', event => {
 });
 
 async function handleRequest(request) {
+  let catFactResponse = await fetch(new Request('https://catfact.ninja/fact'));
+  const myJson = await catFactResponse.json();
+
   const response = await fetch(request)
-  console.log('request is ', request)
   let html = await response.text()
-  console.log('html is ', html)
-  const customScripts = '<style type="text/css">body{background:red}</style></body>'
-  html = html.replace( /<\/body>/ , customScripts)
+
+  const customMarkup = `<p class="text hero__text" id="hero-text">${ myJson }</p>`
+  html = html.replace( /<\/p class="text hero__text" id="hero-text">/ , customMarkup)
 
   return new Response(html, {
     headers: response.headers
