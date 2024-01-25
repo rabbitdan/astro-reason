f37e053867a2ecd54a9f4bec06f9bb480cb2b445
Dan Fone <dan@hammerheadrabbits.com>
2024-01-25 14:12:57 +0000

    another go at HTMLRewriter

--- functions/index.js
+++ functions/index.js
@@ -4,15 +4,25 @@
 //   return Response.json(myJson);
 // }
 
-export async function handleEvent(event) {
-  const response = await getAssetFromKV(event);
-  return new HTMLRewriter().on('#hero-text', new UserElementHandler()).transform(response);
+// export async function handleEvent(event) {
+//   let catFactResponse = await fetch(new Request('https://catfact.ninja/fact'));
+//   const myJson = await catFactResponse.json();
+//   console.log('myJson is ', myJson);
+//   const response = await getAssetFromKV(event);
+//   return new HTMLRewriter().on('#hero-text', new myJson).transform(response);
+// }
+
+class FactElementHandler {
+  async element(element) {
+    let response = await fetch(new Request('/'));
+    console.log('response is ', response);
+    let catFactResponse = await fetch(new Request('https://catfact.ninja/fact'));
+    //const myJson = await catFactResponse.json();
+    return await catFactResponse.json();
+  }
 }
 
-class UserElementHandler {
-  async element(element) {
-    let response = await fetch(new Request('https://catfact.ninja/fact'));
-    const myJson = await response.json();
-    return Response.json(myJson);
-  }
+export async function handleRequest(req) {
+  const res = await fetch(req);
+  return new HTMLRewriter().on('div#hero-text', new FactElementHandler()).transform(res);
 }
