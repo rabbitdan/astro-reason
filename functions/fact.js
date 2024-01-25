f851fef9dfe866f2e93da202c02d00cd184de164
Dan Fone <dan@hammerheadrabbits.com>
2024-01-25 13:15:21 +0000

    back step

--- functions/fact.js
+++ functions/fact.js
@@ -1,10 +1,5 @@
 export async function onRequestGet() {
   const catResponse = await fetch('https://catfact.ninja/fact');
   const myJson = await catResponse.json();
-  const catFactJson = Response.json(myJson);
-  const parsed = JSON.parse(catFactJson);
-  console.log('parsed is ', parsed);
-  console.log('catFactJson is ', catFactJson.fact);
-
-  return catFactJson.fact;
+  return Response.json(myJson);
 }
