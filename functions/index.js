4b83fa8a3f90517de6b3995e9f0354b632ec8009
Dan Fone <dan@hammerheadrabbits.com>
2024-01-25 15:17:09 +0000

    pruning

--- functions/index.js
+++ functions/index.js
@@ -12,24 +12,22 @@
 //   return new HTMLRewriter().on('#hero-text', new myJson).transform(response);
 // }
 
-import { getAssetFromKV, defaultKeyModifier } from '@cloudflare/kv-asset-handler';
+//import { getAssetFromKV, defaultKeyModifier } from '@cloudflare/kv-asset-handler';
 
 addEventListener('fetch', event => {
+  console.log('eventListener runs');
   event.respondWith(handleRequest(event));
 });
 
 class FactElementHandler {
   async element(element) {
     console.log(`Incoming element: ${element.tagName}`);
-    let catFactResponse = await fetch(new Request('https://catfact.ninja/fact'));
-    return await catFactResponse.json();
   }
 }
 
 export async function handleRequest(req) {
   console.log('req is ', req);
   let catFactResponse = await fetch(new Request('https://catfact.ninja/fact'));
-  let catFact = await catFactResponse.json();
-  const res = await fetch(req);
+  const res = await catFactResponse.json();
   return new HTMLRewriter().on('div#hero-text', new FactElementHandler()).transform(res);
 }
