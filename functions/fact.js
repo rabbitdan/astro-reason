07019033678edd9954d93b57315be6e688f514b8
Dan Fone <dan@hammerheadrabbits.com>
2024-01-25 10:20:14 +0000

    stringify

--- functions/fact.js
+++ functions/fact.js
@@ -2,5 +2,6 @@ export async function onRequestGet() {
   const fact = await fetch('https://catfact.ninja/fact');
   const myJson = await fact.json();
   console.log('myJson is ', myJson);
+  myJson.stringify(myJson);
   return new Response(myJson);
 }
