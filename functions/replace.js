22ff6b4c5d74543d2336760e693802357bfb9d23
Dan Fone <dan@hammerheadrabbits.com>
2024-01-25 12:08:51 +0000

    trying log

--- functions/replace.js
+++ functions/replace.js
@@ -6,7 +6,7 @@ addEventListener('fetch', event => {
 async function handleRequest(request) {
   const response = await fetch(request)
   let html = await response.text()
-
+  console.log('response is ', response);
   // Inject scripts
   const customScripts = '<style type="text/css">body{background:red}</style></body>'
   html = html.replace( /<\/body>/ , customScripts)
