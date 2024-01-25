0cb29564aa5aece5fffb76ef4361241f481c1c46
Dan Fone <dan@hammerheadrabbits.com>
2024-01-25 13:35:37 +0000

    more logging plus adding response

--- functions/fact.js
+++ functions/fact.js
@@ -1,5 +1,7 @@
 export async function onRequestGet(request) {
   console.log('request is ', request);
+  const pageResponse = await fetch(request);
+  console.log('pageResponse is ', pageResponse);
   const catResponse = await fetch('https://catfact.ninja/fact');
   const myJson = await catResponse.json();
   return Response.json(myJson);
