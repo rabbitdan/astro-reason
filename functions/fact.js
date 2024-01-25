ef746008c386a10a1488b5e745245ce393f2d85b
Dan Fone <dan@hammerheadrabbits.com>
2024-01-25 13:05:30 +0000

    parsing

--- functions/fact.js
+++ functions/fact.js
@@ -1,8 +1,10 @@
 export async function onRequestGet() {
   const catResponse = await fetch('https://catfact.ninja/fact');
   const myJson = await catResponse.json();
-  // console.log('myJson is ', myJson);
-  // const parsedResponse = JSON.parse(myJson);
-  // console.log('parsedResponse is ', parsedResponse);
-  return Response.json(myJson);
+  const catFactJson = Response.json(myJson);
+  const parsed = JSON.parse(catFactJson);
+  console.log('parsed is ', parsed);
+  console.log('catFactJson is ', catFactJson.fact);
+
+  return catFactJson.fact;
 }
